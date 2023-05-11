94a2a0256cba033d2d81d304e4f6f11cf6573829
Damian Elenbaas <damianelenbaas2003@gmail.com>
2023-05-11 10:24:06 +0200

    Changed sql settings for remote

--- src/utils/mysql-db.js
+++ src/utils/mysql-db.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'share_a_meal',
+  host: process.env.DB_HOST || '127.0.0.1',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || 'share_a_meal',
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   idleTimeout: 60000,
