e9d6d25e082c612b576687f7925f74a88d67db2b
Damian Elenbaas <66534349+damian-elenbaas@users.noreply.github.com>
2023-05-16 19:22:45 +0200

    Added update meal function

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -91,6 +91,117 @@ meal.create = function (req, res) {
 }
 
 meal.update = function (req, res) {
+  const required = joi.object({
+    isActive: joi.boolean(),
+    isVega: joi.boolean(),
+    isVegan: joi.boolean(),
+    isToTakeHome: joi.boolean(),
+    dateTime: joi.date(),
+    maxAmountOfParticipants: joi.number()
+    .required(),
+    price: joi.number()
+    .required(),
+    imageUrl: joi.string(),
+    cookId: joi.number(),
+    createDate: joi.date(),
+    updateDate: joi.date(),
+    name: joi.string()
+    .required(),
+    description: joi.string(),
+    allergenes: joi.array().items(joi.string.valid('gluten', 'lactose', 'noten'))
+  });
+
+  let mealid = req.params.mealid;
+  let payloadId = res.locals.decoded.id;
+
+  const validation = required.validate(req.body);
+  if(validation.error) {
+    return res.status(400).json({
+      'status': 400,
+      'message': validation.error.details[0].message,
+      'data': {}
+    });
+  }
+
+  let sql = "UPDATE meal SET ";
+  let fieldCount = 0;
+  Object.entries(req.body).forEach(([key, value]) => {
+    const keys = Object.entries(required.describe().keys);
+    keys.forEach(([field, props]) => {
+      if(field == key) {
+        if(fieldCount == 0) {
+          sql = sql + `${field} = ?`;
+        } else {
+          sql = sql + `, ${field} = ?`;
+        }
+        fieldCount = fieldCount + 1;
+      }
+    })
+  })
+  sql = sql + " WHERE id = ?";
+
+  let values = Object.values(req.body);
+  values.push(mealid);
+
+  pool.getConnection((err, conn) => {
+    if(err) {
+      logger.error(err);
+      return res.status(500).json({
+        'status': 500,
+        'message': 'Internal server error',
+        'data': {}
+      });
+    }
+
+    conn.query('SELECT * FROM meal WHERE id = ?', [mealid], (sqlError, sqlResults) => {
+      if(sqlError) {
+        return res.status(500).json({
+          'status': 500,
+          'message': 'Internal server error',
+          'data': {}
+        });
+      }
+
+      if(sqlResults.length == 0) {
+        return res.status(404).json({
+          'status': 404,
+          'message': 'Maaltijd niet gevonden',
+          'data': {}
+        });
+      }
+
+      const m = sqlResults[0];
+      if(m.cookId != payloadId) {
+        return res.status(403).json({
+          'status': 403,
+          'message': 'Je bent niet de eigenaar van de maaltijd',
+          'data': {}
+        });
+      }
+
+      conn.query(
+        sql,
+        values,
+        (sqlError, sqlResults) => {
+          if(sqlError) {
+            return res.status(500).json({
+              'status': 500,
+              'message': 'Internal server error',
+              'data': {}
+            });
+          }
+
+          return res.status(200).json({
+            'status': 200,
+            'message': 'Maaltijd succesvol gewijzigd',
+            'data': {}
+          });
+        }
+      );
+    });
+    pool.releaseConnection(conn);
+  });
+
   res.status(200).json({
     'status': 200,
     'message': 'Not implemented yet',
